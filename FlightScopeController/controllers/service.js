3a904eee6041ce8c4d41edfff2d05295db77a47c
Nick Preiser <nickpreiser@gmail.com>
2021-12-29 13:21:03 -0800

    added python stuff

--- FlightScopeController/controllers/service.js
+++ FlightScopeController/controllers/service.js
@@ -1,10 +1,10 @@
-var WebSocket = require('ws');
-var wss = new WebSocket.Server({ port: 22888 });
+//var WebSocket = require('ws');
+//var wss = new WebSocket.Server({ port: 22888 });
 
-var wsclient = undefined;
+//var wsclient = undefined;
 
 var latestdatabucket = undefined
-wss.on('connection', function connection(ws) {
+/*wss.on('connection', function connection(ws) {
     wsclient = ws;
     // ws.on('message', function incoming(message) {
     //    console.log('received: %s', message);
@@ -13,7 +13,7 @@ wss.on('connection', function connection(ws) {
         console.log('client disconnected');
         wsclient = undefined;
     });
-});
+});  */
 
 
 function dispatchData(data)
@@ -24,6 +24,16 @@ function dispatchData(data)
 
 var service = module.exports = {
 
+    runchildproc: function()
+    {
+        console.log("python path: " + process.env.SCOPE_PYTHON_PATH);
+        console.log("script path: " + process.env.SCOPE_PYTHON_SCRIPT);
+        var spawn = require('child_process').spawn
+       // py = spawn('/home/pi/.virtualenvs/cv/bin/python', ['/home/pi/A_localGit/FlightScopeEyeball/golf_blob.py'],)
+        py = spawn(process.env.SCOPE_PYTHON_PATH, [process.env.SCOPE_PYTHON_SCRIPT],)
+        console.log("Started python process");
+
+    },
      updateClients: function(data)
      {
          dispatchData(data) // send data out to the client web pages that are currently on (via web socket)

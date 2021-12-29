62a7cfcc9eeafb91ce4a41f16dd3fee0baa10c04
Nick Preiser <nickpreiser@gmail.com>
2021-12-29 13:42:43 -0800

    more commits

--- FlightScopeController/app.js
+++ FlightScopeController/app.js
@@ -23,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,6 +39,11 @@ app.use(function(err, req, res, next) {
 });
 
 var service = require('./controllers/service.js');
-service.runchildproc();
+
+if (process.arch == "arm") {
+  service.runchildproc();
+}
+
+console.log("system arch: " + process.arch);
 
 module.exports = app;
